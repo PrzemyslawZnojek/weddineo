8689bc59b6afbb624569076234bf63951b25ee03
PrzemyslawZnojek <przemek.znojek@gmail.com>
2020-10-18 21:14:36 +0200

    first commit

---
+++ src/main/java/com/worm/code/weddineo/WeddineoApplication.java
@@ -0,0 +1,13 @@
+package com.worm.code.weddineo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddineoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeddineoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/worm/code/weddineo/WeddineoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worm.code.weddineo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeddineoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
