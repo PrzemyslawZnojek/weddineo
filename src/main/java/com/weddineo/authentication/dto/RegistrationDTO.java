c6e17f3c7d28c7ee3dee3244e7834ecf3ce56df6
PrzemyslawZnojek <przemek.znojek@gmail.com>
2020-11-18 18:15:48 +0100

    Refactoring after code review

--- src/main/java/com/weddineo/authentication/dto/RegistrationDTO.java
+++ src/main/java/com/weddineo/authentication/dto/RegistrationDTO.java
@@ -1,10 +1,8 @@
 package com.weddineo.authentication.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-@Getter
-@Setter
+@Data
 public class RegistrationInfo {
     private String username;
     private String password;
