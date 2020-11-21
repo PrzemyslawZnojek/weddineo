124e6a5d65b240eb88f925b15a75a86e8c049bc4
Kusibab <kusibab.jack@gmail.com>
2020-11-21 22:52:38 +0100

    add error and auth-token interceptors

--- weddineo-frontend/apps/weddineo-frontend/src/app/app.module.ts
+++ weddineo-frontend/apps/weddineo-frontend/src/app/app.module.ts
@@ -7,6 +7,10 @@ import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 import { VersionModule } from '@weddineo-frontend/version';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { ErrorInterceptor } from './shared/interceptor/error.interceptor';
+import { TokenInterceptor } from './shared/interceptor/token.interceptor';
 
 @NgModule({
   declarations: [AppComponent],
@@ -24,9 +28,12 @@ import { VersionModule } from '@weddineo-frontend/version';
     ),
     EffectsModule.forRoot([]),
     !environment.production ? StoreDevtoolsModule.instrument() : [],
-    VersionModule
+    VersionModule,
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
