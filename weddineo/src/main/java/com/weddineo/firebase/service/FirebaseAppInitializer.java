8e23641d565bdc562149b824f5f4f7cd52ba0883
ukasz <lukas.matuszyk@gmail.com>
2020-11-21 18:33:03 +0100

    Change loadind credentials - use resurce loader

--- weddineo/src/main/java/com/weddineo/firebase/service/FirebaseAppInitializer.java
+++ weddineo/src/main/java/com/weddineo/firebase/service/FirebaseAppInitializer.java
@@ -1,24 +1,27 @@
 package com.weddineo.firebase.service;
 
+import com.google.auth.Credentials;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.weddineo.firebase.exception.WeddineoFirebaseRuntimeException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Component
 @Slf4j
 public class FirebaseAppInitializer {
 
-    @Value("${firebase.private.key.path}")
-    private String firebasePrivateKeyPath;
+    @Value("classpath:firebase/weddineo-an-firebase-adminsdk.json")
+    private Resource firebasePrivateKey;
 
     @Value("${firebase.datebase.url}")
     private String firebaseDatabaseURL;
@@ -49,15 +52,13 @@ public class FirebaseAppInitializer {
 
     private FirebaseOptions createFirebaseOptions() throws IOException {
         FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(createFileInputStream()))
+                .setCredentials(getGoogleCredentials())
                 .setDatabaseUrl(firebaseDatabaseURL)
                 .build();
         return options;
     }
 
-    private FileInputStream createFileInputStream() throws FileNotFoundException {
-        FileInputStream serviceAccount =
-                new FileInputStream(firebasePrivateKeyPath);
-        return serviceAccount;
+    private GoogleCredentials getGoogleCredentials() throws IOException {
+        return GoogleCredentials.fromStream(firebasePrivateKey.getInputStream());
     }
 }
