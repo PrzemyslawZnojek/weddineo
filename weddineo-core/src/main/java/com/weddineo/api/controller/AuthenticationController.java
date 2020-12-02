d9a450a961636937b85a0888f0ca8f41b05cb83c
PrzemyslawZnojek <38556901+PrzemyslawZnojek@users.noreply.github.com>
2020-12-03 00:05:17 +0100

    logout logic

--- weddineo-core/src/main/java/com/weddineo/api/controller/AuthenticationController.java
+++ weddineo-core/src/main/java/com/weddineo/api/controller/AuthenticationController.java
@@ -1,6 +1,7 @@
 package com.weddineo.api.controller;
 
 import com.weddineo.api.dto.RegistrationDTO;
+import com.weddineo.authentication.service.LogoutService;
 import com.weddineo.authentication.service.RegistrationService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -9,25 +10,29 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Api(value = "Authentication",
-        tags = { "Authentication" },
-        description = "API to handle weddineo authentication and authorization" )
+@Api(value = "Authentication", tags = {
+        "Authentication" }, description = "API to handle weddineo authentication and authorization")
 @RestController
 @RequestMapping("/auth")
 public class AuthenticationController {
     private RegistrationService registrationService;
+    private LogoutService logoutService;
 
-    public AuthenticationController(RegistrationService registrationService) {
+    public AuthenticationController(RegistrationService registrationService, LogoutService logoutService) {
         this.registrationService = registrationService;
+        this.logoutService = logoutService;
     }
 
-    @ApiOperation(
-            value = "Authentication",
-            tags = "Version"
-    )
+    @ApiOperation(value = "Authentication", tags = "Version")
     @PostMapping(value = "/register")
-    public RegistrationDTO createUser(@RequestBody RegistrationDTO registrationDTO){
+    public RegistrationDTO createUser(@RequestBody RegistrationDTO registrationDTO) {
         registrationService.createUser(registrationDTO);
         return registrationDTO;
+    }
+
+    @ApiOperation(value = "Authentication", tags = "Version")
+    @PostMapping(value = "/logout")
+    public void logout() {
+        logoutService.logout();
     }
 }
