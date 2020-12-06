8f1739282cf674be639664954dfa4616df6ed4af
PrzemyslawZnojek <przemek.znojek@gmail.com>
2020-12-06 20:53:37 +0100

    Adding GlobalExceptionHandler with one customize handling.

--- weddineo-core/src/main/java/com/weddineo/exception/handler/GlobalExceptionHandler.java
+++ weddineo-core/src/main/java/com/weddineo/exception/handler/GlobalExceptionHandler.java
@@ -1,7 +1,22 @@
 package com.weddineo.exception.handler;
 
+import com.weddineo.exception.dto.WeddineoApiErrorDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<WeddineoApiErrorDTO> illegalArgument(IllegalArgumentException ex, WebRequest request){
+        WeddineoApiErrorDTO weddineoApiErrorDTO = new WeddineoApiErrorDTO(HttpStatus.BAD_REQUEST, "Illegal argument", ex, request);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(weddineoApiErrorDTO);
+    }
+
 }
